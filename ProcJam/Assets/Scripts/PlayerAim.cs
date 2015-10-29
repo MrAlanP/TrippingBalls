d03923af96466bfb3eee0167b79750f415819ad8
shuff264 <jshuff264@gmail.com>
2015-10-29 17:07:49 +0000

    Added an aiming curve, also broke aiming Swings and roundabouts really

--- ProcJam/Assets/Scripts/PlayerAim.cs
+++ ProcJam/Assets/Scripts/PlayerAim.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerAim : MonoBehaviour {
 
@@ -8,17 +9,33 @@ public class PlayerAim : MonoBehaviour {
 	public Player player;
 	public Camera mainCam;
 	public GameObject rubberBand;
+	public GameObject TrajectoryPointPrefab;
+	float power = 5.0f;
 
 	SpriteRenderer spriteRenderer;
 
 
 	float aimAngle = 0;
 
+	private int numOfTrajectoryPoints = 30;
+	private List<GameObject> trajectoryPoints;
 	
 
 	// Use this for initialization
 	void Awake () {
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+
+		trajectoryPoints = new List<GameObject>();
+
+		for (int i=0; i < numOfTrajectoryPoints; i++) {
+
+			GameObject dot = (GameObject) Instantiate (TrajectoryPointPrefab);
+			dot.GetComponent<Renderer>().enabled = false;
+			trajectoryPoints.Insert(i,dot);
+
+
+		}
+
 	}
 	
 	// Update is called once per frame
@@ -45,6 +62,8 @@ public class PlayerAim : MonoBehaviour {
 			
 			aimAngle = Mathf.Atan2 (mousePlayerOffset.y, mousePlayerOffset.x);
 			SetAimPos (aimAngle);
+
+
 		} 
 		else {
 			if(spriteRenderer.enabled){
@@ -52,6 +71,12 @@ public class PlayerAim : MonoBehaviour {
 			}
 			SetSpriteRendererActive(false);
 		}
+
+			Vector3 vel = GetForceFrom(rubberBand.transform.position,Camera.main.ScreenToWorldPoint(Input.mousePosition));
+			float angle = Mathf.Atan2(vel.y,vel.x)* Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3(0,0,angle);
+			setTrajectoryPoints(transform.position, vel/rubberBand.GetComponent<Rigidbody2D>().mass);
+
 
 
         //aimAngle = Mathf.Atan2(joyY*joysensitivityY, joyX*joysensitivityX);
@@ -102,6 +127,30 @@ public class PlayerAim : MonoBehaviour {
 
 	public bool GetIsAiming(){
 		return isAiming;
+	}
+
+	private Vector2 GetForceFrom(Vector3 fromPos, Vector3 toPos){
+
+		return (new Vector2 (toPos.x, toPos.y) - new Vector2 (fromPos.x, fromPos.y)) * power;
+
+	}
+
+	void setTrajectoryPoints(Vector3 pStartPosition , Vector3 pVelocity )
+	{
+		float velocity = Mathf.Sqrt((pVelocity.x * pVelocity.x) + (pVelocity.y * pVelocity.y));
+		float angle = Mathf.Rad2Deg*(Mathf.Atan2(pVelocity.y , pVelocity.x));
+		float fTime = 0;
+		fTime += 0.1f;
+		for (int i = 0 ; i < numOfTrajectoryPoints ; i++)
+		{
+			float dx = velocity * fTime * Mathf.Cos(angle * Mathf.Deg2Rad);
+			float dy = velocity * fTime * Mathf.Sin(angle * Mathf.Deg2Rad) - (Physics2D.gravity.magnitude * fTime * fTime / 2.0f);
+			Vector3 pos = new Vector3(pStartPosition.x + dx , pStartPosition.y + dy ,2);
+			trajectoryPoints[i].transform.position = pos;
+			trajectoryPoints[i].GetComponent<Renderer>().enabled = true;
+			trajectoryPoints[i].transform.eulerAngles = new Vector3(0,0,Mathf.Atan2(pVelocity.y - (Physics.gravity.magnitude)*fTime,pVelocity.x)*Mathf.Rad2Deg);
+			fTime += 0.1f;
+		}
 	}
 
 
