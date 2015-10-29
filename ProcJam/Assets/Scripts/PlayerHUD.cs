70bf021fc168ffb481f145309c423f0cb023eb55
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2015-10-29 21:11:37 +0000

    guessing at rubber band ammo hud

--- ProcJam/Assets/Scripts/PlayerHUD.cs
+++ ProcJam/Assets/Scripts/PlayerHUD.cs
@@ -7,22 +7,35 @@ public class PlayerHUD : MonoBehaviour {
     public GameObject player;
 	public Text rubberBandsCount;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
         //ArrayList bandslist = new ArrayList();
        bandList = GameObject.FindGameObjectsWithTag("bandTower");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
+        
        
-       {
-
-       }
+           
+      
 
 	
 	}
 
 	public void UpdateRubberBandsCount(int count){
 		rubberBandsCount.text = count.ToString();
+        for (int i=0;i>count;i++)
+        {
+            bandList[i].SetActive(true);
+                //GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if (bandList.Length>count)
+        {
+            bandList[bandList.Length - count].SetActive(false);
+        }
+
+
 	}
 }
