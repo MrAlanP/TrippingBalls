70d36f4f37e3f8205f2f97c65dbaffc40fdd974e
shuff264 <jshuff264@gmail.com>
2015-10-30 16:49:39 +0000

    Basic Health Bar plus a test collider
    Theres a test collider just to the right of the player, run into an take
    damage.
    
    All the fun of real pain with out the pain

---
+++ ProcJam/Assets/DamageTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageTest : MonoBehaviour {
+
+	PlayerHealth playerHealth;
+	int attackDamage = 20;
+	GameObject player;
+
+	void Awake(){
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.gameObject == player) {
+
+			Debug.Log("PLAYER HIT");	
+			playerHealth.takeDamage(attackDamage);
+
+		}
+	}
+
+
+//	void OnTriggerEnter(Collider other){
+//
+//		Debug.Log("OVERLAP");
+//
+//		if (other.gameObject == player) {
+//
+//			Debug.Log("PLAYER HIT");	
+//			playerHealth.takeDamage(attackDamage);
+//
+//		}
+
+
+//	}
+
+
+}
---
+++ ProcJam/Assets/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider slider;
+
+	bool isDead;
+
+
+	// Use this for initialization
+	void Start () {
+
+		currentHealth = startingHealth;
+	
+	}
+	
+	public void takeDamage(int amount){
+
+		currentHealth -= amount;
+
+		slider.value = currentHealth;
+
+		if (currentHealth <= 0 && !isDead) {
+
+			isDead = true;
+		}
+
+
+	}
+}
