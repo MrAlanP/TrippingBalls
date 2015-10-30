49221859ccc45bd6f8277c9e4326516377f9ca99
shuff264 <jshuff264@gmail.com>
2015-10-30 18:09:34 +0000

    Fixed an error, all should work

--- ProcJam/Assets/Scripts/Damage.cs
+++ ProcJam/Assets/Scripts/Damage.cs
@@ -1,43 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DamageTest : MonoBehaviour {
-
+public class Damage : MonoBehaviour {
+	
 	PlayerHealth playerHealth;
-	int attackDamage = 20;
+	public int attackDamage = 20;
 	GameObject player;
-
+	
 	void Awake(){
-
+		
 		player = GameObject.FindGameObjectWithTag ("Player");
 		playerHealth = player.GetComponent <PlayerHealth> ();
-
+		
 	}
-
+	
 	void OnTriggerEnter2D(Collider2D other) {
-
+		
 		if (other.gameObject == player) {
-
+			
 			Debug.Log("PLAYER HIT");	
 			playerHealth.takeDamage(attackDamage);
-
+			
 		}
 	}
+	
+	
 
-
-//	void OnTriggerEnter(Collider other){
-//
-//		Debug.Log("OVERLAP");
-//
-//		if (other.gameObject == player) {
-//
-//			Debug.Log("PLAYER HIT");	
-//			playerHealth.takeDamage(attackDamage);
-//
-//		}
-
-
-//	}
-
-
+	
 }
