68915c036743e19cbd4b5ee644d2c656b08510d7
shuff264 <jshuff264@gmail.com>
2015-10-30 19:39:34 +0000

    Basis of the rubber band ammo HUD is in
    Its hudderly ridiculous

---
+++ ProcJam/Assets/RubberBandCounter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class RubberBandCounter : MonoBehaviour {
+
+	List<GameObject> rubberBandsHud;
+	public GameObject[] rubberBands;
+
+	// Use this for initialization
+	void Start () {
+
+//		rubberBandsHud = new List<GameObject>();
+//
+//		rubberBandsHud.Add(GameObject.FindGameObjectsWithTag("bandTower"));
+	
+		rubberBands = GameObject.FindGameObjectsWithTag("bandTower");
+
+		Debug.Log(rubberBands.Length);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for(int i = 0; i < 3; i++)
+		{
+			rubberBands[i].SetActive(false);
+		}
+	
+	}
+}
