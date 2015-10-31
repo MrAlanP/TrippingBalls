670115546e82ae8d8495654328f83076762769e3
Alan Parsons <klaxorx@gmail.com>
2015-10-31 12:59:53 +0000

    Helmet damage sprites and fixed enemybounce prefab missing script

--- ProcJam/Assets/Scripts/Damage.cs
+++ ProcJam/Assets/Scripts/Damage.cs
@@ -3,21 +3,18 @@ using System.Collections;
 
 public class Damage : MonoBehaviour {
 	
-	PlayerHealth playerHealth;
-	public int attackDamage = 20;
-	GameObject player;
+
 	
 	void Awake(){
-		
-		player = GameObject.FindGameObjectWithTag ("Player");
-		playerHealth = player.GetComponent <PlayerHealth> ();
+
 		
 	}
 	
 	void OnTriggerEnter2D(Collider2D other) {
 		
-		if (other.gameObject == player) {
-			playerHealth.takeDamage(attackDamage);
+		if (other.gameObject.GetComponent<Player>()) {
+			PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+			playerHealth.takeDamage();
 		}
 	}
 	
--- ProcJam/Assets/Scripts/PlayerHealth.cs
+++ ProcJam/Assets/Scripts/PlayerHealth.cs
@@ -4,25 +4,35 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
 
-	public int startingHealth = 100;
-	public int currentHealth;
-	public Slider slider;
+	const int START_HEALTH = 3;
+	int currentHealth;
+	public Slider healthSlider;
+
+	public SpriteRenderer helmetSprite;
+
+	public Sprite[] helmetDamageSprites;
 
 	bool isDead;
 
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 
-		currentHealth = startingHealth;
-	
+		currentHealth = START_HEALTH;
+		if (helmetDamageSprites.Length > 0) {
+			helmetSprite.sprite = helmetDamageSprites [0];
+		}
+
 	}
 	
-	public void takeDamage(int amount){
+	public void takeDamage(){
 
-		currentHealth -= amount;
-
-		slider.value = currentHealth;
+		currentHealth--;
+		currentHealth = Mathf.Clamp(currentHealth,0,START_HEALTH);
+		if (helmetDamageSprites.Length > 0) {
+			helmetSprite.sprite = helmetDamageSprites [3-currentHealth];
+		}
+		healthSlider.value = ((float)currentHealth/(float)START_HEALTH) * 100;
 
 		if (currentHealth <= 0 && !isDead) {
 
