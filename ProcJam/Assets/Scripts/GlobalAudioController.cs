1bb1cba1ff368338566d4cd5c857f50a66ef20dc
Liam-Young <Liam2.Young@live.uwe.ac.uk>
2015-11-02 12:51:55 +0000

    Some Menu Updates
    Beginning menu flesh

---
+++ ProcJam/Assets/Scripts/GlobalAudioController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GlobalAudioController : MonoBehaviour {
+
+	public static GlobalAudioController Instance;
+	public float MusicVolume;
+
+	// Use this for initialization
+	void Start () {
+	
+		if (Instance == null) {
+			DontDestroyOnLoad (gameObject);
+			Instance = this;
+		}
+		else if (Instance != this){
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
