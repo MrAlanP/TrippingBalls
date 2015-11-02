0fb3f29b27ca0268752ee4a750e36bc3f540cefe
Alan Parsons <a24-parsons@STU-CZC3280QDG.campus.ads.uwe.ac.uk>
2015-11-02 13:37:24 +0000

    Blood particles

---
+++ ProcJam/Assets/Scripts/ChangeParticleLayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeParticleLayer : MonoBehaviour {
+	public string sortingLayer = "Foreground";
+	// Use this for initialization
+	void Awake () {
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = sortingLayer; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
